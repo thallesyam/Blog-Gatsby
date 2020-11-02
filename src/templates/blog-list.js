e590d9b90c60dc2d13bf168973b8c24f86daf298
Thalles Ian <thallesyam@users.=.github.com>
2020-11-02 14:22:02 -0300

    corrigindo querys

--- src/templates/blog-list.js
+++ src/templates/blog-list.js
@@ -63,29 +63,29 @@ const BlogList = props => {
 }
 
 export const query = graphql`
-  query PostList($skip: Int!, $limit: Int!) {
-    allMarkdownRemark(
-      sort: { fields: frontmatter___date, order: DESC }
-      limit: $limit
-      skip: $skip
-    ) {
-      edges {
-        node {
-          fields {
-            slug
-          }
-          frontmatter {
-            background
-            category
-            date(locale: "pt-br", formatString: "DD [de] MMMM [de] YYYY")
-            description
-            title
-          }
-          timeToRead
+query PostList($skip: Int!, $limit: Int!) {
+  allMarkdownRemark(
+    sort: { fields: frontmatter___date, order: DESC }
+    limit: $limit
+    skip: $skip
+  ) {
+    edges {
+      node {
+        fields {
+          slug
         }
+        frontmatter {
+          background
+          category
+          date(locale: "pt-br", formatString: "DD [de] MMMM [de] YYYY")
+          description
+          title
+        }
+        timeToRead
       }
     }
   }
+}
 `
 
 export default BlogList
